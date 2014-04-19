029ceb3ccaa894a601babddc9582c1e42aba0931
Brannon McGraw <bmcgraw@gwmail.gwu.edu>
2014-04-19 16:21:00 -0400

    Created catalog page, reformatted item detail page. TODO: use HR page as reference for the catalog table with links

--- themes/bootstrap/views/item/catalog.php
+++ themes/bootstrap/views/item/catalog.php
@@ -1,7 +1,56 @@
 <?php
+$connection = Yii::app()->db;
+$all_items_query = "select item_id, name, price, sale_price, release_date, SUM(quantity)
+                              from item i, store_item s 
+                              where i.item_id = s.store_item_id
+			      group by item_id";
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+		   
+		 
+$all_items_command = $connection->createCommand($all_items_query);
+
+$all_items = $all_items_command->queryAll();
+
+$itemarray = array();
+
+foreach ($all_items as $item){
+    if( isset($item['sale_price']) ){
+	$itemarray[] = array(
+      	    'id' => $item['item_id'],
+	    'name' => $item['name'],
+            'price' =>$item['sale_price'],
+	    'release_date' => $item['release_date'],
+	    'quantity' => $item['SUM(quantity)']
+	    );
+
+    }else{
+        $itemarray[] = array(
+            'id' => $item['item_id'],
+	    'name' => $item['name'],
+            'price' =>$item['price'],
+	    'release_date' => $item['release_date'],
+	    'quantity' => $item['SUM(quantity)']
+	    );
+    }
+}
+
+$gridDataProvider_item = new CArrayDataProvider($itemarray);
 ?>
+
+<h1 class="page-header">Site Catalog</h1>
+<legent>Item Catalog</legent>
+<?php
+$this->widget('bootstrap.widgets.BsGridView', array(
+    'dataProvider' => $gridDataProvider_item
+    ,
+    'id' => uniqid('table_'),
+    'columns' => array(
+	array('name' => 'name', 'header' => 'Name'),
+	array('name' => 'price', 'header' => 'Price'),
+	array('name' => 'release_date', 'header' => 'Available Starting'),
+	array('name' => 'quantity', 'header' => 'Quantity available'),
+    ),
+    'type' => BSHtml::GRID_TYPE_STRIPED
+));
+?>
+
--- themes/bootstrap/views/item/item.php
+++ themes/bootstrap/views/item/item.php
@@ -1,4 +1,9 @@
 <?php
+if(!isset($_GET['item_id'])){
+    header( 'Location: catalog' );
+    die();
+}
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -10,11 +15,10 @@ $connection = Yii::app()->db;
 ****************************************************/
 
 $safe_id;
-if (isset($_GET['item_id'])){
+if(isset($_GET['item_id']))
     $safe_id = mysql_real_escape_string($_GET['item_id']);
-}
 
-if (isset($safe_id) ) {
+if(isset($safe_id)){
     $item_info_query = "select name, price, release_date, sale_price 
 		   	   from item 
 			   where item_id = $safe_id";
@@ -23,9 +27,29 @@ if (isset($safe_id) ) {
     $item_info_command->reset();
 
     $item_quantity_query = "SELECT SUM(quantity) from store_item 
-    			   where item_store_id = $safe_id)";
+    			   where item_store_id = $safe_id";
     $item_quantity_command = $connection->createCommand($item_quantity_query);
-    $item_quantity = $item_quantity_command->queryAll();
+    $item_quantity = $item_quantity_command->queryRow();
     $item_quantity_command->reset();
 }
+ 
 ?>
+<h1 class="page-header">Item detail</h1>
+<br/>
+<br/>
+<?php if(!strcmp($item_info['name'], '')): ?>
+   Internal error: Item not found
+   <br/>
+   Return to <a href='catalog'>Item Catalog?</a>
+   
+<?php else:  ?>
+
+<h4>Item: <?= $item_info['name']?></h4>
+<h4>Price: $<?=number_format($item_info['price'], 2, '.', ' '); ?></h4>
+<h4>Quantity: <?= $item_quantity['SUM(quantity)'] ?></h4>
+<? if( isset($_POST['location']) ){
+
+}
+?> 
+
+<?php endif; ?>
