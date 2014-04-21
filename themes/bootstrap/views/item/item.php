7647e2296887f53db92be44982c912a7df3c78c6
Brannon McGraw <bmcgraw@gwmail.gwu.edu>
2014-04-21 15:59:08 -0400

    Added forward and backward search on item detail page

--- themes/bootstrap/views/item/item.php
+++ themes/bootstrap/views/item/item.php
@@ -14,9 +14,7 @@ $connection = Yii::app()->db;
 *  Queries for this page
 ****************************************************/
 
-$safe_id;
-if(isset($_GET['item_id']))
-    $safe_id = mysql_real_escape_string($_GET['item_id']);
+$safe_id = mysql_real_escape_string($_GET['item_id']);
 
 if(isset($safe_id)){
     $item_info_query = "select name, price, release_date, sale_price 
@@ -47,9 +45,13 @@ if(isset($safe_id)){
 <h4>Item: <?= $item_info['name']?></h4>
 <h4>Price: $<?=number_format($item_info['price'], 2, '.', ' '); ?></h4>
 <h4>Quantity: <?= $item_quantity['SUM(quantity)'] ?></h4>
-<? if( isset($_POST['location']) ){
-
-}
-?> 
-
 <?php endif; ?>
+<h6>
+<?php
+  $prev = $safe_id - 1;
+  $next = $safe_id +1;
+  echo "<a href=item?item_id=".$prev.">previous item</a>";
+  echo "| <a href=item?item_id=" . $next . ">next item</a>";
+
+?>
+</h6>
