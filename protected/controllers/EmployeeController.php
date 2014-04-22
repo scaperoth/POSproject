a3fcbf7a8289536a2fd0f5f5c7d98ff9b05c4890
Brannon McGraw <bmcgraw@gwmail.gwu.edu>
2014-04-22 13:41:16 -0400

    pushing latest changes

--- protected/controllers/EmployeeController.php
+++ protected/controllers/EmployeeController.php
@@ -8,14 +8,6 @@ class EmployeeController extends Controller {
             ),
         );
     }
-<<<<<<< HEAD
-   
-    public function actionIndex() {
-        $this->render('employee');
-    }
-
-=======
->>>>>>> 34d7e762171b9fc55003f1f16f96fd2070a03117
     /*     * **********************
      *     EMPLOYEE PAGES
      * ********************** */
@@ -24,89 +16,48 @@ class EmployeeController extends Controller {
         // using the default layout 'protected/views/layouts/main.php'
         $this->render('employee');
     }
-    public function actionCheckout() {
-<<<<<<< HEAD
-      if(isset($_POST["employeeForm"])){
-            //validate credentials
-          $loginForm = $_POST['employeeForm'];
-          $username =  $loginForm['username']; 
-          $pass = $loginForm['password'];
-          $emp_id = Yii::app()->user->id; 
-          $connection = Yii::app()->db;
-          $isThisAUserQuery = "Select user_id from user 
-	 		  where user_id = ".$emp_id." 
-			  and pass = \"".$pass."\"and username = \"".$username."\""; 
-         $users=$connection->createCommand($isThisAUserQuery)->queryAll();
-         if($users){
-             $havePermissionQuery = "Select * from has_permissions where usr_id =".$emp_id." and permission_id > 1";
-             $permissions = $connection->createCommand($havePermissionQuery)->queryAll(); 
-             if($permissions){
-	     	/*      Do checkout         */
-                
-                
-     	     }else{ 
-	         echo("That user is in our system, but doesn't have permission to check out a customer");
-             }
-         }else{
-             echo("That username and password combo is incorrect"); 
-         }
 
-        }
-        $this->render('checkout');
-=======
+    public function actionCheckout() {
         if(isset($_POST["saleForm"])){
             //validate credentials
-          $saleForm = $_POST['saleForm'];
-          $username =  $saleForm['emp_username']; 
-          $pass = $saleForm['emp_password'];
+            $saleForm = $_POST['saleForm'];
+            $username =  $saleForm['emp_username']; 
+            $pass = $saleForm['emp_password'];
+            $connection = Yii::app()->db;
+            $isThisAUserQuery = "Select * from user where pass = \"$pass\" and username = \"$username\""; 
+            $employee = $connection->createCommand($isThisAUserQuery)->queryRow();
 
 
-      $connection = Yii::app()->db;
+            if($employee){
+                $havePermissionQuery = "Select * from has_permissions where usr_id = ".$employee['user_id']." and permission_id > 1";
+                $permissions = $connection->createCommand($havePermissionQuery)->queryRow(); 
+                if(!empty($permissions)) {
+                    //this is the store id for the employee that just logged in
+                    $store_id = Works::model()->findByAttributes(array("store_emp_id"=>$employee['user_id']));
 
-      $isThisAUserQuery = "Select * from user where pass = \"".$pass."\" and username = \"".$username."\""; 
-
-      $employee = $connection->createCommand($isThisAUserQuery)->queryRow();
-
-
-
-
-      if($employee){
-          $havePermissionQuery = "Select * from has_permissions where usr_id = ".$employee['user_id']." and permission_id > 1";
-          $permissions = $connection->createCommand($havePermissionQuery)->queryRow(); 
-          if(!empty($permissions)) {
-                //this is the store id for the employee that just logged in
-                $store_id = Works::model()->findByAttributes(array("store_emp_id"=>$employee['user_id']));
-
-                //insert sale using employee id, $employee['user_id'], customer id from the form, $saleForm['user_id'] and the same for the item id
-                $customer_id = $saleForm['cust_id'];
-                $item_id = $saleForm['item_id'];
-
-
-
-                if($item_id != -1){
-                  $newSale = new Sale();
-                  $newSale->sale_cust_id = $customer_id; 
-                  $newSale->sale_emp_id = $employee['user_id']; 
-                  $newSale->sale_store_id = $store_id['employee_store_id'];
-                  $newSale->sale_item_id = $item_id; 
-                  $newSale->save();
-
-                   Yii::app()->user->setFlash("success","This sale has been made");
-             $this->redirect(array('/site/'));
-
-                } else {
-                  echo("A sale with no item was submitted, but not processed. Try Again!");
-                }
-
-                
-          } else echo("That user is in our system, but doesn't have permission to check out a customer"); 
-      }
-      else echo("That username and password combo is incorrect"); 
-      // using the default layout 'protected/views/layouts/main.php'
+                    //insert sale using employee id, $employee['user_id'], customer id from the form, $saleForm['user_id'] and the same for the item id
+                    $customer_id = $saleForm['cust_id'];
+                    $item_id = $saleForm['item_id'];
+                    if($item_id != -1){
+                        $newSale = new Sale();
+                        $newSale->sale_emp_id = $employee['user_id']; 
+                        $newSale->sale_cust_id = $customer_id; 
+                        $newSale->sale_store_id = $store_id['employee_store_id'];
+                        $newSale->sale_item_id = $item_id; 
+                        $newSale->save();
+                        Yii::app()->user->setFlash("success","This sale has been made");
+                        $this->redirect(array('/site/'));
+                    }else{
+                        echo("A sale with no item was submitted, but not processed. Try Again!");
+                    }
+                } else 
+	            echo("That user is in our system, but doesn't have permission to check out a customer"); 
+            }else 
+	        echo("That username and password combo is incorrect"); 
+                // using the default layout 'protected/views/layouts/main.php'
+        }
+        $this->render('checkout');
     }
-     $this->render('checkout');
->>>>>>> 34d7e762171b9fc55003f1f16f96fd2070a03117
-}
     
 // Uncomment the following methods and override them if needed
     /*
