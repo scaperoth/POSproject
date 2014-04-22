0de5b4d54305ac062562c27a3701363919ea9f29
root <root@ubuntu.(none)>
2014-04-22 07:35:36 -0700

    pushing employee changes

--- protected/controllers/EmployeeController.php
+++ protected/controllers/EmployeeController.php
@@ -8,11 +8,6 @@ class EmployeeController extends Controller {
             ),
         );
     }
-    
-    public function actionIndex() {
-        $this->render('employee');
-    }
-
     /*     * **********************
      *     EMPLOYEE PAGES
      * ********************** */
@@ -23,44 +18,55 @@ class EmployeeController extends Controller {
     }
 
     public function actionCheckout() {
-        if(isset($_POST["employeeForm"])){
+        if(isset($_POST["saleForm"])){
             //validate credentials
-          $loginForm = $_POST['employeeForm'];
-          $username =  $loginForm['username']; 
-          $pass = $loginForm['password'];
+          $saleForm = $_POST['saleForm'];
+          $username =  $saleForm['emp_username']; 
+          $pass = $saleForm['emp_password'];
 
-      $user_id = Yii::app()->user->id; 
 
       $connection = Yii::app()->db;
 
-      $isThisAUserQuery = "Select user_id from user where user_id = ".$user_id." and pass = \"".$pass."\" and username = \"".$username."\""; 
+      $isThisAUserQuery = "Select * from user where pass = \"".$pass."\" and username = \"".$username."\""; 
 
-      $users=$connection->createCommand($isThisAUserQuery)->queryAll();
+      $employee = $connection->createCommand($isThisAUserQuery)->queryRow();
 
-      if($users){
-        $havePermissionQuery = "Select * from has_permissions where usr_id =".$user_id." and permission_id > 1";
-        $permissions = $connection->createCommand($havePermissionQuery)->queryAll(); 
-        if($permissions) die();
-        else echo("That user is in our system, but doesn't have permission to check out a customer"); 
 
+
+
+      if($employee){
+          $havePermissionQuery = "Select * from has_permissions where usr_id = ".$employee['user_id']." and permission_id > 1";
+          $permissions = $connection->createCommand($havePermissionQuery)->queryRow(); 
+          if(!empty($permissions)) {
+                //this is the store id for the employee that just logged in
+                $store_id = Works::model()->findByAttributes(array("store_emp_id"=>$employee['user_id']));
+
+                //insert sale using employee id, $employee['user_id'], customer id from the form, $saleForm['user_id'] and the same for the item id
+                $customer_id = $saleForm['cust_id'];
+                $item_id = $saleForm['item_id'];
+
+
+
+                if($item_id != -1){
+                  $newSale = new Sale();
+                  $newSale->sale_cust_id = $customer_id; 
+                  $newSale->sale_emp_id = $employee['user_id']; 
+                  $newSale->sale_store_id = $store_id['employee_store_id'];
+                  $newSale->sale_item_id = $item_id; 
+                  $newSale->save();
+
+                   Yii::app()->user->setFlash("success","This sale has been made");
+             $this->redirect(array('/site/'));
+
+                } else {
+                  echo("A sale with no item was submitted, but not processed. Try Again!");
+                }
+
+                
+          } else echo("That user is in our system, but doesn't have permission to check out a customer"); 
       }
-      else {
-        echo("That username and password combo is incorrect"); 
-
-      }
-
-
-         
-
-        
-
-          
-    
-
-          
-        // using the default layout 'protected/views/layouts/main.php'
-       
-
+      else echo("That username and password combo is incorrect"); 
+      // using the default layout 'protected/views/layouts/main.php'
     }
      $this->render('checkout');
 }
