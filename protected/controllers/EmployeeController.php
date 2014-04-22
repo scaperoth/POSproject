c6295d10effe698246578162afca289e85b35466
mgoldiner <mgoldiner@yahoo.com>
2014-04-21 22:11:29 -0400

    added flash for when sale is made

--- protected/controllers/EmployeeController.php
+++ protected/controllers/EmployeeController.php
@@ -16,54 +16,55 @@ class EmployeeController extends Controller {
     }
 
     public function actionCheckout() {
-        if(isset($_POST["employeeForm"])){
+        if(isset($_POST["saleForm"])){
             //validate credentials
-          $loginForm = $_POST['employeeForm'];
-          $username =  $loginForm['username']; 
-          $pass = $loginForm['password'];
+          $saleForm = $_POST['saleForm'];
+          $username =  $saleForm['emp_username']; 
+          $pass = $saleForm['emp_password'];
 
 
       $connection = Yii::app()->db;
 
       $isThisAUserQuery = "Select * from user where pass = \"".$pass."\" and username = \"".$username."\""; 
 
-      $users = $connection->createCommand($isThisAUserQuery)->queryAll();
-
-      if($users){
-        foreach($users as $user){
-           $havePermissionQuery = "Select * from has_Permissions where usr_id = ".$user['user_id']." and permission_id > 1";
-          $permissions = $connection->createCommand($havePermissionQuery)->queryAll(); 
-          if($permissions) {
-           echo("Success");
-           $this->render("checkoutScreen");
-           die();  
-          }else echo("That user is in our system, but doesn't have permission to check out a customer"); 
-        }
+      $employee = $connection->createCommand($isThisAUserQuery)->queryRow();
 
 
+
+
+      if($employee){
+          $havePermissionQuery = "Select * from has_permissions where usr_id = ".$employee['user_id']." and permission_id > 1";
+          $permissions = $connection->createCommand($havePermissionQuery)->queryRow(); 
+          if(!empty($permissions)) {
+                //this is the store id for the employee that just logged in
+                $store_id = Works::model()->findByAttributes(array("store_emp_id"=>$employee['user_id']));
+
+                //insert sale using employee id, $employee['user_id'], customer id from the form, $saleForm['user_id'] and the same for the item id
+                $customer_id = $saleForm['cust_id'];
+                $item_id = $_GET['item_id'];
+
+
+
+                if($item_id != -1){
+                  $newSale = new Sale();
+                  $newSale->sale_cust_id = $customer_id; 
+                  $newSale->sale_emp_id = $employee['user_id']; 
+                  $newSale->sale_store_id = $store_id['store_emp_id'];
+                  $newSale->sale_item_id = $item_id; 
+                  $newSale->save();
+
+                   Yii::app()->user->setFlash("Success","This sale has been made");
+             Yii::app()->getController()->redirect(array('/site/'));
+
+                } else {
+                  echo("A sale with no item was submitted, but not processed. Try Again!");
+                }
+
+                
+          } else echo("That user is in our system, but doesn't have permission to check out a customer"); 
       }
-      else {
-        foreach($users as $a) {
-          echo($a['pass']);
-        } 
-        echo("That username and password combo is incorrect"); 
-        
-
-
-      }
-
-
-         
-
-        
-
-          
-    
-
-          
-        // using the default layout 'protected/views/layouts/main.php'
-       
-
+      else echo("That username and password combo is incorrect"); 
+      // using the default layout 'protected/views/layouts/main.php'
     }
      $this->render('checkout');
 }
